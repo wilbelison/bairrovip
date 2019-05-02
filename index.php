254bafb17ccf0745cddfbc5283f36d1e0978e703
Wilbelison Junior <meustudio@gmail.com>
2019-05-02 17:37:14 -0300

    ZIP packs

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <title>Bairro VIP Generator</title>
     <meta charset="UTF-8" />
     <link rel="stylesheet" type="text/css" href="libs/style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,900" rel="stylesheet">
   </head>
   <body>
 
@@ -17,19 +18,16 @@ delete_files('temp/');
 
 if(isset($_POST['submit'])) {
 
-?>
-
-<ul id="arquivos">
-
-<?php
-
   mkdir('temp/728x90/');
   mkdir('temp/300x250/');
   mkdir('temp/320x100/');
 
+  mkdir('temp/src/');
+
   // FOTOS RESIZE
 
   $nome = $_POST['nome'];
+  $slug = slugify($nome);
 
   $countfiles = count($_FILES['fotos']['name']);
 
@@ -39,13 +37,11 @@ if(isset($_POST['submit'])) {
 
     $filename = $_FILES['fotos']['name'][$i];
     $filesize = $_FILES['fotos']['size'][$i];
-    move_uploaded_file($_FILES['fotos']['tmp_name'][$i],'temp/' . $filename);
+    move_uploaded_file($_FILES['fotos']['tmp_name'][$i],'temp/src/' . $filename);
 
-    echo '<li>' . $filename . ' <span class="size">' . filesize_formatted($filesize) . '</span> <span class="ok">ok</span> </li>';
-
-    resize_crop_image(224, 90, 'temp/' . $filename, 'temp/728x90/foto_' . $i . '.jpg');
-    resize_crop_image(300, 110, 'temp/' . $filename, 'temp/300x250/foto_' . $i . '.jpg');
-    resize_crop_image(150, 100, 'temp/' . $filename, 'temp/320x100/foto_' . $i . '.jpg');
+    resize_crop_image(224, 90, 'temp/src/' . $filename, 'temp/728x90/foto_' . $i . '.jpg');
+    resize_crop_image(300, 110, 'temp/src/' . $filename, 'temp/300x250/foto_' . $i . '.jpg');
+    resize_crop_image(150, 100, 'temp/src/' . $filename, 'temp/320x100/foto_' . $i . '.jpg');
 
     $fotos .= "'foto_" . $i . ".jpg'";
 
@@ -59,17 +55,13 @@ if(isset($_POST['submit'])) {
 
   $filename = $_FILES['logo']['name'];
   $filesize = $_FILES['logo']['size'];
-  move_uploaded_file($_FILES['logo']['tmp_name'],'temp/' . $filename);
+  move_uploaded_file($_FILES['logo']['tmp_name'],'temp/src/' . $filename);
 
-  echo '<li>' . $filename . ' <span class="size">' . filesize_formatted($filesize) . '</span> <span class="ok">ok</span> </li>';
-
-  resize_crop_image(162, 90, 'temp/' . $filename, 'temp/728x90/logo.png');
-  resize_crop_image(280, 45, 'temp/' . $filename, 'temp/300x250/logo.png');
-  resize_crop_image(124, 80, 'temp/' . $filename, 'temp/320x100/logo.png');
+  resize_crop_image(172, 90, 'temp/src/' . $filename, 'temp/728x90/logo.png');
+  resize_crop_image(290, 45, 'temp/src/' . $filename, 'temp/300x250/logo.png');
+  resize_crop_image(124, 80, 'temp/src/' . $filename, 'temp/320x100/logo.png');
 
   $nome = $_POST['nome'];
-
-  
 
   $hash = array(
     'nome' => $nome,
@@ -85,19 +77,28 @@ if(isset($_POST['submit'])) {
   copy('templates/320x100/vivareal.png', 'temp/320x100/vivareal.png');
   copy('templates/320x100/seta.gif', 'temp/320x100/seta.gif');
 
+  mkdir('temp/zip/');
+
+  ZIP('temp/728x90/', 'temp/zip/' . $slug . '-728x90.zip');
+  ZIP('temp/300x250/', 'temp/zip/' . $slug . '-300x250.zip');
+  ZIP('temp/320x100/', 'temp/zip/' . $slug . '-320x100.zip');
+
+  $download = './temp/' . $slug . '.zip';
+
+  ZIP('temp/zip/', $download);
+
 ?>
 
-</ul>
-
 <div id="preview">
-  <iframe class="w728h90" src="temp/728x90/index.html"></iframe>
-  <iframe class="w300h250" src="temp/300x250/index.html"></iframe>
+  <iframe class="w728h90" src="temp/728x90/index.html"></iframe><br />
+  <iframe class="w300h250" src="temp/300x250/index.html"></iframe><br />
   <iframe class="w320h100" src="temp/320x100/index.html"></iframe>
 </div>
 
 <ul id="acoes">
-  <li class="download"><a href="download.zip">Download</a> ou</li>
-  <li class="new"><a href="index.php">Criar um novo banner</a></li>
+  
+  <li class="new"><a href="index.php">← Criar novo banner</a> ou</li>
+  <li class="download"><a href="<?php echo $download ?>">⤓ Download</a></li>
 </ul>
 
 <?php } else { ?>
@@ -116,13 +117,15 @@ if(isset($_POST['submit'])) {
         <label>Logo:  <input type="file" name="logo" id="logo" required="required"></label>
       </p>
 
-      <p><input type='submit' name='submit' value='Enviar'></p>
+      <p><input type='submit' name='submit' value='Enviar →' id="enviar"></p>
 
     </form>
 
+    <script>document.getElementById('nome').focus();</script>
+
 <?php } ?>
 
-  <p id="copyright">Desenvolvido com <i>❤</i> por <a href="mailto:meustudio@gmail.com">Wilbelison Junior</a></p>
+  <p id="copyright">Desenvolvido com <i>❤</i> por <a href="http://bit.ly/wilbelison" target="_blank">Wilbelison Junior</a></p>
 
   </body>
 </html>
--- libs/functions.php
+++ libs/functions.php
@@ -102,4 +102,66 @@ function filesize_formatted($size) {
     return number_format($size / pow(1024, $power), 2, '.', ',') . ' ' . $units[$power];
 }
 
+// ZIP FILES
+
+function ZIP($source, $destination)
+{
+    if (extension_loaded('zip') === true)
+    {
+        if (file_exists($source) === true)
+        {
+            $zip = new ZipArchive();
+
+            if ($zip->open($destination, ZIPARCHIVE::CREATE) === true)
+            {
+                $source = realpath($source);
+
+                if (is_dir($source) === true)
+                {
+                    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+                    foreach ($files as $file)
+                    {
+                        $file = realpath($file);
+
+                        if (is_dir($file) === true)
+                        {
+                            $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+                        }
+
+                        else if (is_file($file) === true)
+                        {
+                            $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+                        }
+                    }
+                }
+
+                else if (is_file($source) === true)
+                {
+                    $zip->addFromString(basename($source), file_get_contents($source));
+                }
+            }
+
+            return $zip->close();
+        }
+    }
+
+    return false;
+}
+
+// SLUG
+
+function slugify($text) {
+  $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+  $text = preg_replace('~[^-\w]+~', '', $text);
+  $text = trim($text, '-');
+  $text = preg_replace('~-+~', '-', $text);
+  $text = strtolower($text);
+  if (empty($text)) {
+    return 'n-a';
+  }
+  return $text;
+}
+
 ?>
